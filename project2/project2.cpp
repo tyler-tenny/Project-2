c6bbfa1d7e4ef8c816ef009730029bea4c6768fb
Tyler Tenny <tyler.tenny@oit.edu>
2022-01-29 17:40:37 -0800

    jan 29, add project docs and findnumdigits

--- project2/project2.cpp
+++ project2/project2.cpp
@@ -6,7 +6,10 @@ void DisplayMenu();
 void ProcessMenuChoice(int&);
 int GetData(int userint = 0);
 int IsPosNeg(int userint = 0);
-
+int IsOddEven(int x = 0);
+int FindNumDigits(int userint = 0);
+int FindDigitAtPosition(int userint = 0);
+//Main function - by Tyler
 int main()
 {
 	//Show options//
@@ -22,11 +25,16 @@ int main()
 	case 1:
 		IsPosNeg();
 		break;
-	/*case 2:*/
+	case 2:
+		IsOddEven();
+		break;
+	case 3:
+		FindNumDigits();
+		break;
 	}
-	return Selection;
+	return 0;
 }
-//Complete
+//Complete - by Tyler
 void DisplayMenu()
 {
 	cout << "Welcome! \n Please select an option from the menu below. \n\n"; 
@@ -37,13 +45,13 @@ void DisplayMenu()
 	cout << "\n\n(5) DisplayAdditionTable: Displays the addition table for a specified number from 1-10";
 	cout << "\n\n(6) DisplayMultiplicationTable: Displays the multiplication table for a specified number from 1-10\n\n";
 }
-//Complete
+//Complete in call by reference form - by Tyler
 void ProcessMenuChoice(int& Selection)
 {
 	//Collect menu choice//
 	cin >> Selection;
 }
-//IsPosNeg is complete in call by value form//
+//IsPosNeg is complete in call by value form - by Tyler//
 int IsPosNeg(int userint)
 {
 	userint = GetData(userint);
@@ -55,9 +63,38 @@ int IsPosNeg(int userint)
 		cout << userint << " is NEGATIVE." << endl;
 	return 0;
 }
-
-
-//GetData is complete in call by value form//
+//IsOddEven complete in call by value form - by Christian
+int IsOddEven(int x)
+{
+	x = GetData(x);
+	if (x % 2 == 0)
+		cout << x << " is even.";
+	else
+		cout << x << " is odd.";
+	return 0;
+}
+//FindNumDigits complete in call by value form - by Tyler//
+int FindNumDigits(int userint)
+{
+	userint = GetData(userint);
+	int inttemp = userint;
+	int digits = 0;
+	if (inttemp < 0)
+		inttemp *= -1;
+	while (inttemp > 0)
+	{
+		inttemp/=10;
+		digits++;
+	}
+	if (digits == 1)
+		cout << userint << " has " << digits << " digit in it.";
+	else
+		cout << userint << " has " << digits << " digits in it.";
+	return 0;
+}
+// FindDigitAtPosition
+// 
+//GetData is complete in call by value form - by Tyler//
 int GetData(int userint)
 {
 	cout << "Please enter an integer between one million and negative one million." << endl;
